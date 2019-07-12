3bc38afd79115ffa0919b167cc3b40a805921c91
nicholashaynes1 <nicholashaynes1@gmail.com>
2019-07-11 18:56:49 -0600

    Added my different algorithms for evaluating data.

--- main.py
+++ main.py
@@ -42,3 +42,34 @@ dataset = pandas.read_csv(url, names = names)
 #scatter plots
 # scatter_matrix(dataset)
 # plt.show()
+
+array = dataset.values
+X = array[:,0:4]
+Y = array[:,4]
+validationSize = 0.20
+seed = 7
+
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X,Y, test_size = validationSize, random_state = seed)
+
+# Test options
+seed = 7
+scoring = 'accuracy'
+
+
+models = []
+models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC(gamma='auto')))
+# evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+	kfold = model_selection.KFold(n_splits=10, random_state=seed)
+	cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+	results.append(cv_results)
+	names.append(name)
+	msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+	print(msg)
